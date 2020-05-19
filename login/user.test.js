707d20b953acd3033f806d365858608c507d4363
Gois <matheusgoislimasilva@gmail.com>
2020-05-18 22:42:52 -0300

    lats tests is done

--- login/user.test.js
+++ login/user.test.js
@@ -8,8 +8,8 @@ const UserModel = {
   _id: '507f191e810c19729de860ea',
 };
 
-describe('', function() {
-  it('Read Users', function(done) {
+describe('Users', function() {
+  it('Read All', function(done) {
     request(app)
         .get('/api/user/get/all')
         .set('Accept', 'application/json')
@@ -20,13 +20,49 @@ describe('', function() {
           done();
         });
   });
-  it('Create User', function(done) {
+  it('Create', function(done) {
     request(app)
         .post('/api/user/create')
         .send(UserModel)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        // .expect(200)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+  });
+  it('Authenticate', function(done) {
+    request(app)
+        .post('/api/user/auth')
+        .send(UserModel)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+  });
+  it('Delete by _id', function(done) {
+    request(app)
+        .delete(`/api/user/delete/${UserModel._id}`)
+        .send(UserModel)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+  });
+  it('Delete all', function(done) {
+    request(app)
+        .delete(`/api/user/delete`)
+        .send(UserModel)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
         .end(function(err, res) {
           if (err) return done(err);
           done();
